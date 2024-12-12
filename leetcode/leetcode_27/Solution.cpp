779fc5d7c319545a463b10d62658949af49f0888
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-12-12 06:38:36 -0600

    update

---
+++ leetcode/leetcode_27/Solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+    }
+};
