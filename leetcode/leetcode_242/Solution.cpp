24cb7ff91ad2f42ac32f25fbb3dc00f8c2092a10
Manh Cuong Do - Pi 5 <mdo@miu.edu>
2024-12-23 11:37:51 -0600

    add leetcode_242 valid anagram

---
+++ leetcode/leetcode_242/Solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+int main() {
+    string s = "anagram";
+    string t = "nagaram";
+    Solution* solution = new Solution();
+    cout << solution->isAnagram(s, t) << endl;
+    s = "car";
+    t = "cat";
+    cout << solution->isAnagram(s, t) << endl;
+}
